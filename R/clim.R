c886ecb3a550ca697a502e50aed55c248dacdf90
sofiemccomb <shemccomb@gmail.com>
2019-06-06 17:59:44 -0700

    Working on clim documentation

---
+++ R/clim.R
@@ -0,0 +1,21 @@
+#' Climate Data from Santa Barbara
+#'
+#' Data from SB-LTER metereology station, daily
+#' meterologic data including minimum temperature,
+#' maximum temperature, precipitation, water year,
+#' and the date
+#'
+#' @format A data frame with 7920 rows and 9 variables:
+#' \itemize{
+#' \item D Year-Month-Day
+#' \item day
+#' \item month
+#' \item year
+#' \item wy wateryear
+#' \item tmax_c maximum temperature (C)
+#' \item tmin_c minimum temperature (C)
+#' \item precip precipitation (mm/day)
+#' \item wyd water year day
+#' }
+#'
+#' @source \
