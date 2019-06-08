cd4050264447b4cee5c8cc20f74a0f61ac98ba1c
msleckman <msleckman@ucsb.edu>
2019-06-08 14:30:18 -0700

    impact function description rough

---
+++ R/Impact_function.R
@@ -0,0 +1,12 @@
+#' Function 5: Impact
+#' @title
+#' @description something about air conditioning and temperature (min or max or mean)
+#' @param
+#' @param
+#' @param
+#' @author Sofie McComb & Margaux Sleckman
+#' @example
+#' @return
+#'
+#'
+#'
