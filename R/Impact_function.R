f2e7bace253d70d62f44f5d8c7e3c0b76d272a0f
msleckman <msleckman@ucsb.edu>
2019-06-11 18:55:19 -0700

    impact

--- R/Impact_function.R
+++ R/Impact_function.R
@@ -1,21 +1,26 @@
-#' Function 5: Impact
-#' @title
-#' @description something about air conditioning and temperature (min or max or mean)
-#' @param
-#' @param
-#' @param
+#' Function 5: Heating and Cooling Cost Function
+#' @title Heating and Cooling Cost Function
+#' @description This function takes climate data with daily minimum and maximum temperatures and returns the daily cost of air conditions and heating.
+#' @concepts The equation used to calculate the heating and cooling cost goes as the following: We assume the user will turn on heating if the temperature maximum is 8 degrees C. We assume the user will turn on AC if the temperature minimum is 14 degrees. Simply turning on the AC or the heater costs $10, and every additional degree above the baseline (8 C or 14 C) costs a specified price_per_degree. The default is 0.8, but this can be change by manipulating the appropriate param.
+#' @param climatedata This is where you put in a climate dataset.
+#' @param price_heating_per_degree this is the cost of every incremental increasing in heating due to the difference in actual heating with the baseline.
+#' @param price_cooling_per_degree same as above, but for cooling.
 #' @author Sofie McComb & Margaux Sleckman
-#' @example
-#' @return
-#'
+#' @example calculate_heating_AC_cost(climatedata = clim, price_heating_per_degree = 0.8,
+#' price_cooling_per_degree = 0.8, interactive_plot_selection = T)
+#' @return a list of the dataframe with date, month, year, and the cost of heating and cooling for that day.
 
 
-library(dplyr)
-library(tidyverse)
 
-heating_airtemp_function <- function(climatedata,
-                                     price_heating_degree = 0.8, price_cooling_per_degree = 0.8,
-                                     airconditioning = T, heating = T){
+calculate_heating_AC_cost <- function(climatedata,
+                                     price_heating_per_degree = 0.8, price_cooling_per_degree = 0.8,
+                                     interactive_plot_selection = T){
+
+# Library
+  library(dplyr)
+  library(tidyverse)
+  if(!require(ggiraph))install.packages("ggiraph")
+  library(ggiraph)
 
 ## $10 is the baseline if you turn on your heater  on or air conditioning
 ## We assume heater turns on when the tmax is below or equal to 8 degrees C.
@@ -24,40 +29,41 @@ heating_airtemp_function <- function(climatedata,
 ## Baseline temp for air conditioning == 14 C
 ## With every degree increase above or below the baseline temp, the cost goes up 0.8 cents. So, you will spend $10.8 if the average outdoor temp is 15 degrees.
 
-
 clim_cost <- climatedata %>%
-  mutate(average_temp = (tmin+tmax)/2) %>%
-  mutate(aircond_price = ifelse(tmin >= 14, (10 + abs(14 - average_temp * price_cooling_per_degree)), 0)) %>%
-  mutate(heating_price = ifelse(tmax <= 8, (10 + abs(8 - average_temp * price_heating_degree)), 0))
-
+  dplyr::mutate(average_temp = (tmin+tmax)/2) %>%
+  dplyr::mutate(aircond_price = ifelse(tmin >= 14, (10 + abs(14 - average_temp * price_cooling_per_degree)), 0)) %>%
+  mutate(heating_price = ifelse(tmax <= 8, (10 + abs(8 - average_temp * price_heating_per_degree)), 0))
 
 AC_heating_per_month <- clim_cost %>%
-  group_by(date, year, month) %>%
-  summarise(average_monthly_AC_cost = mean(aircond_price),
+  dplyr::group_by(date, year, month) %>%
+  dplyr::summarise(average_monthly_AC_cost = mean(aircond_price),
                                            average_monthly_heating_cost = mean(heating_price))
 
+AC_heating_per_month_for_plot <- subset(AC_heating_per_month, average_monthly_AC_cost != 0 |
+                                         average_monthly_heating_cost != 0)
 
-plot <- ggplot(AC_heating_per_month)+
-  geom_point(aes(x=month,  y = average_monthly_AC_cost), col="blue", alpha = 0.3)+
-  geom_point(aes(x=month,  y = average_monthly_heating_cost), col = "red", alpha = 0.3)+
+
+plot <- ggplot(AC_heating_per_month_for_plot)+
+  ggiraph::geom_point_interactive(aes(x=month,  y = average_monthly_AC_cost, tooltip = date), col="blue", alpha = 0.3)+
+  ggiraph::geom_point_interactive(aes(x=month,  y = average_monthly_heating_cost, tooltip = date), col = "red", alpha = 0.3)+
   theme_classic()+
   xlab("Month")+
-  ylab("average cost of temperature regulation")+
-  labs(title = "Datapoints of Monthly temperature regulation cost between 1942-2015"
+  ylab("average cost of temperature regulation ($)")+
+  labs(title = "Datapoints of Monthly temperature regulation cost\n between 1942-2015"
        # subtitle = sprintf(price_heating_degree, price_cooling_per_degree)
-       )
+  )+
+  theme(axis.text.x.bottom = )
 
-# install.packages("plotly")
-# library(plotly)
-#
-# gg <- ggplotly(plot)
-#
-# gg <- style(gg, line = list(color = 'gold'), hoverinfo = "date")
-#
-# gg
+interactive_plot <- girafe(code = print(plot))
 
-return(list(AC_heating_per_month, plot))
+if(interactive_plot_selection == T){
+  return(list(Dataframe = AC_heating_per_month, Plot = interactive_plot))
+  }
+
+else{
+  return(list(Dataframe = AC_heating_per_month))
+  }
+
 }
 
 
-heating_airtemp_function(clim)
