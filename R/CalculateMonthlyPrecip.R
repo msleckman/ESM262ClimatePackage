f2167cbd90b37c42b60c6e35ae531c02657a1969
msleckman <msleckman@ucsb.edu>
2019-06-12 01:31:33 -0700

    tests MORE

--- R/CalculateMonthlyPrecip.R
+++ R/CalculateMonthlyPrecip.R
@@ -13,11 +13,11 @@
 #' @author Sofie McComb & Margaux Sleckman
 #'
 
-CalculateMonthlyPrecip <- function(clim_data){
+CalculateMonthlyPrecip <- function(clim_data, full_clim_dataset = T){
 
 # Clim dataframe modification
 
-  clim_w_week <- clim %>%
+  clim_w_week <- clim_data %>%
   dplyr::mutate(week = ifelse(day <= 7, 1,
                        ifelse(day > 7 & day <= 14, 2,
                               ifelse(day > 14 & day <= 21, 3,
@@ -25,13 +25,15 @@ CalculateMonthlyPrecip <- function(clim_data){
                                      )))) %>% transform(id=match(year, unique(year)))
 
 years_working <-  length(unique(clim_w_week$year))-1
+months_working <- length(unique(clim_w_week$month))
+weeks_working <- length(unique(clim_w_week$week))
 
-clim_array = array(dim = c(4, 12, years_working))
+clim_array = array(dim = c(weeks_working, months_working, years_working))
 
 ## Populate with mean weekly values
 
-for (weeks in 1:4){
-  for (months in 1:12){
+for (weeks in 1:weeks_working){
+  for (months in 1:months_working){
     for (years in 1:years_working){
 
         value <- clim_w_week %>%
@@ -44,12 +46,16 @@ for (weeks in 1:4){
     }
   }
 
-# add  names
+# add  names if size is apprepriate
+if(full_clim_dataset == T){
+
 dimnames(clim_array) = list(c("week 1","week 2","week 3","week 4"),
                        c("Jan","Feb", "Mar", "Apr", "May", "Jun",
                           "Jul","Aug", "Sep", "Oct","Nov", "Dec"),
                        c(seq(from = 1942,to = 2015, by = 1))
                          )
+
+}
 
 ## average precip by year
 average_precip_by_year = apply(clim_array, c(3), mean)
@@ -61,10 +67,8 @@ average_precip_by_month = apply(clim_array, c(2), mean)
 average_precip_by_month <- as.data.frame(average_precip_by_month)
 
 
-
 return(list(`average precipitation by month` = average_precip_by_month,
             `average precipitation per year` = average_precip_by_year))
-
 
 }
 
--- tests/testthat/test_ComputeHeatingAC_Cost.R
+++ tests/testthat/test_ComputeHeatingAC_Cost.R
@@ -1,12 +1,7 @@
 
-library(ggplot2)
-library(ggiraph)
+test_that("ComputeHeatingAC_Cost works",{
 
-source("R/CalculateMonthlyPrecip.R")
-source('R/ComputeHeatingAC_Cost.R')
-
-
-clim_test_data =
+  clim_test_data =
   as.data.frame(
     cbind(
       date = c("01/01/19",
@@ -14,26 +9,19 @@ clim_test_data =
                "03/01/19",
                "04/01/19",
                "04/01/19"),
-      tmin  = runif(min = -3, max = 8, n = 5),
-      tmax  = runif(min = 8, max = 18, n = 5),
+      tmin  = c(1,5,8,9,4),
+      tmax  = c(10,12,17,18,10),
       rain  = rep(1, times = 5),
       year  = rep(1, times = 5),
       month = rep(1, times = 5),
       day   = rep(1, times = 5),
       wy = rep(1942, times = 5)))
 
-)
-
 clim_test_data <- clim_test_data %<>% mutate_if(is.factor,as.numeric)
 
-clim_test_data<-sapply(clim_test_data, as.numeric)
-str(clim_test_data)
+expect_that(ComputeHeatingAC_Cost(clim_test_data)$meanHeating_cost, equals(16))
+expect_that(ComputeHeatingAC_Cost(clim_test_data)$meanAC_cost, equals(0))
 
-
-expect_that(ComputeHeatingAC_Cost(clim_test_data)$meanHeating_cost, equals(15.55))
-
-
-aircond_price = ifelse(~tmin >= 14, (10 + abs(14 - ~average_temp * ~price_cooling_per_degree
-str(clim_test_data)
+})
 
 
